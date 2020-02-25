2bc103066831b35e0a18ee4cb6751202f01f4303
MThuro <mareike.thurau@t-online.de>
2020-02-25 08:23:14 +0100

    Update color picker + rename operation

--- order-app/src/app/material.module.ts
+++ order-app/src/app/material.module.ts
@@ -14,6 +14,7 @@ import { MatDatepickerModule,
   MatButtonModule, 
   MatIconModule, 
   MatListModule,
+  MatSnackBar,
   MatExpansionModule } from '@angular/material';
   import {FormsModule, ReactiveFormsModule, FormGroup} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
--- order-app/src/app/process-creation/process-creation.component.ts
+++ order-app/src/app/process-creation/process-creation.component.ts
@@ -1,3 +1,4 @@
+import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
 import { OrderService } from './../order.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,13 +12,19 @@ import { Process } from './../process';
 export class ProcessCreationComponent implements OnInit {
   orderID: string;
 
-  constructor(private router: Router, private orderService: OrderService) { }
+  constructor(private router: Router, private orderService: OrderService, public snackbar: MatSnackBar) { }
 
   ngOnInit() {
     this.orderID = this.orderService.getOrderID().toString();
   }
 
   add(id: string, color: string): void {
+    if (!id){
+      this.snackbar.open("Please enter all mandatory fields", "Dismiss",{
+        duration: 3000,
+      });
+      return;
+    }
     let process = new Process();    
     process.id = id;
     process.color = color;
